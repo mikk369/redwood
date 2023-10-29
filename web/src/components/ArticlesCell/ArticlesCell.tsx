cda4d636c3726216a643251f1fade8ab40663ee4
mikk369 <mikk.ereline@vikk.ee>
2023-10-29 19:46:19 +0200

    table styles

--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -1,52 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import { TreeTable } from 'primereact/treetable';
-import { Column } from 'primereact/column';
-import { InputText } from 'primereact/inputtext';
-import { SelectButton } from 'primereact/selectbutton';
-import { TreeNode } from 'primereact/treenode';
-import { NodeService } from './../../service/NodeService';
+import React, { useState, useEffect } from 'react'
 
-interface FilterModeOption {
-    label: string;
-    value: string;
-}
+import { Column } from 'primereact/column'
+import { InputText } from 'primereact/inputtext'
+import { TreeNode } from 'primereact/treenode'
+import { TreeTable } from 'primereact/treetable'
+
+import { NodeService } from './../../service/NodeService'
+import 'primeicons/primeicons.css'
 
 export default function FilterDemo() {
-    const [nodes, setNodes] = useState<TreeNode>([]);
-    const [globalFilter, setGlobalFilter] = useState<string>('');
-    const [filterMode, setFilterMode] = useState('lenient');
-    const [filterOptions] = useState<FilterModeOption[]>([
-        { label: 'Lenient', value: 'lenient' },
-        { label: 'Strict', value: 'strict' }
-    ]);
+  const [nodes, setNodes] = useState<TreeNode>([])
+  const [globalFilter, setGlobalFilter] = useState<string>('')
 
-    useEffect(() => {
-        NodeService.getTreeTableNodes().then((data) => setNodes(data));
-    }, []);
+  useEffect(() => {
+    NodeService.getTreeTableNodes().then((data) => setNodes(data))
+  }, [])
 
-    const getHeader = () => {
-        return (
-            <div className="flex justify-content-end">
-                <div className="p-input-icon-left">
-                    <i className="pi pi-search"></i>
-                    <InputText type="search" onInput={(e) => setGlobalFilter(e.target.value)} placeholder="Global Search" />
-                </div>
-            </div>
-        );
-    };
-
-    let header = getHeader();
-
+  const getHeader = () => {
     return (
-        <div className="card">
-            <div className="flex justify-content-center mb-4">
-                <SelectButton value={filterMode} onChange={(e) => setFilterMode(e.value)} options={filterOptions} />
-            </div>
-            <TreeTable value={nodes} globalFilter={globalFilter} header={header} filterMode={filterMode} tableStyle={{ minWidth: '50rem' }}>
-                <Column field="name" header="Name" expander filter filterPlaceholder="Filter by name"></Column>
-                <Column field="size" header="Size" filter filterPlaceholder="Filter by size"></Column>
-                <Column field="type" header="Type" filter filterPlaceholder="Filter by type"></Column>
-            </TreeTable>
+      <div className="flex justify-content-end">
+        <div className="p-input-icon-left ">
+          <i className="pi pi-search" style={{ paddingLeft: '1rem' }}></i>
+          <InputText
+            type="search"
+            onInput={(e) => setGlobalFilter(e.target.value)}
+            placeholder="Global Search"
+            style={{ textAlign: 'right' }}
+          />
         </div>
+      </div>
     )
+  }
+
+  const header = getHeader()
+
+  return (
+    <div className="card">
+      <TreeTable
+        value={nodes}
+        globalFilter={globalFilter}
+        header={header}
+        tableStyle={{ minWidth: '50rem' }}
+      >
+        <Column
+          field="name"
+          header="Name"
+          expander
+          filter
+          filterPlaceholder="Filter by name"
+        ></Column>
+        <Column
+          field="size"
+          header="Size"
+          filter
+          filterPlaceholder="Filter by size"
+        ></Column>
+        <Column
+          field="type"
+          header="Type"
+          filter
+          filterPlaceholder="Filter by type"
+        ></Column>
+      </TreeTable>
+    </div>
+  )
 }
